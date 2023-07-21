a8f0b2e4afd0c7e8ac9ab8b367f4c050af569c16
Pablo Assis <52177225+PabloJr84@users.noreply.github.com>
2023-07-21 16:28:11 -0300

    https://github.com/PabloJr84/Meu_Blog.git

--- Pagina_Pablo/mail/contact.php
+++ Pagina_Pablo/mail/contact.php
@@ -13,8 +13,12 @@ $to = "pablo.assis1984@gmail.com"; // Change this email to your //
 $subject = "$m_subject:  $name";
 $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
 $header = "From: $email";
-$header .= "Reply-To: $email";	
+$header .= "Reply-To: $email";
+
+
+	
 
 if(!mail($to, $subject, $body, $header))
   http_response_code(500);
+
 ?>
