9ce5b0f4a5b64f0a43570a3cf8e18c1b70291b6b
PABLO JUNIOR MOREIRA DE ASSIS <52177225+PabloJr84@users.noreply.github.com>
2023-08-20 23:58:30 -0300

    Php e envio de e-mail feito com sucesso

--- Pagina_Pablo/contact.php
+++ Pagina_Pablo/contact.php
@@ -1,24 +1,34 @@
 <?php
-$submitted = false;
-$message = "Obrigado";
+$servername = "localhost:3306";
+$username = "inovac68_Pablo";
+$password = "P@blo161207";
+$dbname = "inovac68_blogpa";
 
+// Cria a conexão
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verifica a conexão
+if ($conn->connect_error) {
+    die("Conexão com o banco de dados falhou: " . $conn->connect_error);
+}
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST["name"];
     $email = $_POST["email"];
     $subject = $_POST["subject"];
-    $messageText = $_POST["message"];
+    $message = $_POST["message"];
 
     // Realize validações e processamento adicional dos dados, se necessário
 
     $to = "contato@studioinovacoes.com.br"; // Endereço de e-mail de destino
     $headers = "From: $email";
-    $messageToSend = "Nome: $name\nE-mail: $email\nAssunto: $subject\nMensagem: $messageText";
+    $messageToSend = "Nome: $name\nE-mail: $email\nAssunto: $subject\nMensagem: $message";
 
     if (mail($to, $subject, $messageToSend, $headers)) {
-        header("Location: "); // Redireciona para a página de sucesso
-        exit; // Termina o script após o redirecionamento
+        // Redirecionamento para a página de agradecimento
+        header("Location: sucesso.html");
+        exit(); // Certifique-se de sair para evitar qualquer saída adicional
     } else {
-        $message = "Erro ao enviar o e-mail.";
+        echo "Erro ao enviar o e-mail.";
     }
 }
 ?>
